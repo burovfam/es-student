0e5d0934ba70dc2f9ce63f412d7e047f8c88d7c3
Василий Шеховцов <shekhovtsov.vm@phystech.edu>
2026-09-19 20:33:04 +0300

    Сделана п1.2.1

--- 112-blink/main.c
+++ 112-blink/main.c
@@ -1,18 +1,24 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
+#include "hardware/regs/addressmap.h"
+#include "hardware/regs/sio.h"
 
 const uint LED_PIN = 25;
 
 int main()
 {
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+   gpio_init(LED_PIN);
+   gpio_set_dir(LED_PIN, GPIO_OUT);
 
-    while (1)
-    {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(1000);
-    }
+   volatile uint32_t *gpio_out_set = (uint32_t *)(SIO_BASE + SIO_GPIO_OUT_SET_OFFSET);
+   volatile uint32_t *gpio_out_clr = (uint32_t *)(SIO_BASE + SIO_GPIO_OUT_CLR_OFFSET);
+   const uint32_t led_mask = 1u << LED_PIN;
+
+   while (1)
+   {
+       *gpio_out_set = led_mask;
+       sleep_ms(250);
+       *gpio_out_clr = led_mask;
+       sleep_ms(1000);
+   }
 }
